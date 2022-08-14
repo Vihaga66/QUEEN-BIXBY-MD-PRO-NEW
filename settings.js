de71958d95990177e28b3e69d2aa2eeb2329949e
Vihaga66 <109607940+Vihaga66@users.noreply.github.com>
2022-08-14 22:15:34 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94711421243']
-global.premium = ['94711421243']
-global.ownernomer = '94711421243'
-global.ownername = 'Dark Alpha'
-global.botname = 'BIXBY-MD USER BOT'
-global.footer = ' Qᴜᴇᴇɴ ʙɪxʙʏ ᴘᴜʙʟɪᴄ ᴇᴅɪᴛɪᴏɴ.'
-global.ig = 'https://github.com/darkalphaxteam'
+global.owner = ['Hi 🤭']
+global.premium = ['Hi 🤭']
+global.ownernomer = 'Hi 🤭'
+global.ownername = 'VIHAGA BOT'
+global.botname = 'VIHAGA BOT'
+global.footer = ' VIHAGA SITHUM.'
+global.ig = '💛 👉 YouTube - SUBSCRIBE click 👈 💛'
 global.region = 'India, South India, Kerala'
-global.sc = 'https://github.com/darkalphaxteam/QUEEN-BIXBY-MD-PRO-NEW'
-global.myweb = 'https://youtube.com/channel/UCvpyOCBCaBSCdv4QolnO0WQ'
-global.packname = 'Queen Bixby MD'
-global.author = 'DARK ALPHA'
+global.sc = 'https://youtube.com/channel/UC3ZwkFVKgeCY9BEA-cdutwA'
+global.myweb = 'https://youtube.com/channel/UC3ZwkFVKgeCY9BEA-cdutwA'
+global.packname = 'vihaga sithum MD'
+global.author = 'vihags sithum'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '🚢'
